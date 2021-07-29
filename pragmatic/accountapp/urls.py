6bbcf4603e6ae5e9ecf80b1d79cb16950436c6a0
hyunhee-lee <hyunhee.lee@vntgcorp.com>
2021-07-30 00:07:42 +0900

    장고 24강

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView
 
 app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
-path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
-path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 ]
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 # Create your views here.
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 from django.urls import reverse, reverse_lazy
@@ -36,3 +36,8 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
