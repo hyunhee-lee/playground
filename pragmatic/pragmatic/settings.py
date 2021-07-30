e77857fcce7d616a5d8f4c0012493617bb0b4149
hyunhee-lee <hyunhee.lee@vntgcorp.com>
2021-07-30 23:09:10 +0900

    장고 29강

--- pragmatic/pragmatic/settings.py
+++ pragmatic/pragmatic/settings.py
@@ -145,6 +145,10 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
