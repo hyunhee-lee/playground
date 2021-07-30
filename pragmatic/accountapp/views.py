420fd0b49243f42865180c8238ac59c378a9ba84
hyunhee-lee <hyunhee.lee@vntgcorp.com>
2021-07-30 20:54:42 +0900

    Bug Fix

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -45,11 +45,13 @@ class AccountDetailView(DetailView):
 
 class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
