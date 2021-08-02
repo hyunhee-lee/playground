c2486bf31ae328ded4e112798e3565b27acb7947
hyunhee-lee <hyunhee.lee@vntgcorp.com>
2021-08-02 21:34:07 +0900

    django 38 course commit

---
+++ pragmatic/commentapp/decorators.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseForbidden
+
+from articleapp.models import Article
+
+
+def comment_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        comment = Comment.objects.get(pk=kwargs['pk'])
+        if not comment.writer == request.user:
+            return HttpResponseForbidden()
+        return func(request, *args, **kwargs)
+    return decorated
