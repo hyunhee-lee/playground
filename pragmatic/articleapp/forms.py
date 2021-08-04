abcd9aa4f342260e4db2ab2bec71cf6884a5ff81
hyunhee-lee <hyunhee.lee@vntgcorp.com>
2021-08-04 17:30:53 +0900

    django course 44 commit

--- pragmatic/articleapp/forms.py
+++ pragmatic/articleapp/forms.py
@@ -1,9 +1,15 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left',
+                                                           'style': 'height: auto; text-align: left;'}))
+
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
