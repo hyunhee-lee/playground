c1c184aa43d1652c902355c51d29addc41610ea0
hyunhee-lee <hyunhee.lee@vntgcorp.com>
2021-08-06 01:34:14 +0900

    bugfix

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -46,7 +46,8 @@ has_ownership = [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
@@ -66,7 +67,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/update.html'
 
     # def get(self, *args, **kwargs):
