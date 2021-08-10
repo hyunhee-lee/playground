363715d759bcc936f451e31f07a43cf3849b33a5
hyunhee-lee <hyunhee.lee@vntgcorp.com>
2021-08-10 23:15:24 +0900

    CHG: about bootstrap ver, etc

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -47,7 +47,7 @@ class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     # success_url = reverse_lazy('accountapp:hello_world')
-    success_url = reverse_lazy('accountapp:login')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
